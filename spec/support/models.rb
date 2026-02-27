174ab8b4d47c4598cc7446a9ce86061a4b9f9f5e
David Kearney <112392744+davidkclio@users.noreply.github.com>
2026-02-27 08:20:03 +0000

    removing some code

--- spec/support/models.rb
+++ spec/support/models.rb
@@ -9,14 +9,7 @@ class ContactBook < ActiveRecord::Base
   has_many :parents
   has_many :children, through: :parents
 
-  has_many_aggregate :companies, :count, :count, "*"
-  has_many_aggregate :companies, :count_with_max_ids_set, :count, "*", max_ids_per_query: 2
-  has_many_aggregate :employees, :count, :count, "*"
-  has_many_aggregate :company_employees, :count, :count, "*"
-  has_many_aggregate :children, :count, :count, "*"
-
   has_many :companies_with_blank_email_address, -> { joins(:email_address).where(email_addresses: { address: "" }) }, class_name: "Company"
-  has_many_aggregate :companies_with_blank_email_address, :count, :count, "*", table_alias_name: "contacts"
 end
 
 class Contact < ActiveRecord::Base
@@ -27,10 +20,6 @@ class Contact < ActiveRecord::Base
   has_many :phone_numbers
   has_one :email_address
   has_many :employees
-
-  has_many_aggregate :addresses, :max_street_length, :maximum, "LENGTH(street)"
-  has_many_aggregate :phone_numbers, :count, :count, "id"
-  has_many_aggregate :addresses, :count, :count, "*"
 
   scope :desc, ->{ order(id: :desc) }
 end
@@ -74,15 +63,9 @@ class Country < ActiveRecord::Base
   has_many :addresses
   has_many :contacts, through: :addresses
   has_many :contact_owners, through: :contacts, source_type: 'ContactOwner'
-
-  has_many_aggregate :contacts, :count, :count, "*"
-  has_many_aggregate :contact_owners, :count, :count, "*"
 end
 
 class ContactOwner < ActiveRecord::Base
   has_many :contacts, as: :contact_owner
   has_many :addresses, through: :contacts
-
-  has_many_aggregate :contacts, :count, :count, "*"
-  has_many_aggregate :addresses, :count, :count, "*"
 end
